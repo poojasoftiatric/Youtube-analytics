0dfc616e36c2d818b9631cbe8f9f8d9436ecd662
poojaSoftiatric <poojasoftiatric@gmail.com>
2025-03-11 17:06:37 +0530

    Youtube-Analytics

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import YouTubeAnalyticsDashboard from './components/YouTubeAnalyticsDashboard'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <YouTubeAnalyticsDashboard />
+  </React.StrictMode>,
+)
